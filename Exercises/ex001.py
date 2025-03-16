c75d535b4419f3b8d510c82db6f0e7caa80d1357
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2025-03-16 13:53:27 +0300

    Update ex001.py

--- Exercises/ex001.py
+++ Exercises/ex001.py
@@ -1,2 +1,9 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543003
+# Your Name and Surname:Abdullah Bakla
+
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are!")
+print("\t\tUp above the world so high,")
+print("\t\tLike a diamond in the sky.")
+print("Twinkle, twinkle, little star,")
+print("\tHow I wonder what you are")
