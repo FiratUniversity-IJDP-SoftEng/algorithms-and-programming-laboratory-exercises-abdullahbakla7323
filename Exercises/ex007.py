9fc8fb1d98b273089286f2af5d8fc1bea5505408
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2025-03-16 14:36:51 +0300

    Update ex007.py

--- Exercises/ex007.py
+++ Exercises/ex007.py
@@ -1,2 +1,13 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543003
+# Your Name and Surname:Abdullah Bakla
+
+
+user_input = input("Enter a string: ")
+
+char_count = {}
+for char in user_input:
+    char_count[char] = char_count.get(char, 0) + 1
+
+print("Character frequencies:")
+for char in sorted(char_count):
+    print(f"{char}: {char_count[char]}")
