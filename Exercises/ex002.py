ac5c33f09e47d82852d321ad546cb78db9fcf410
abdullahbakla7323 <153926191+abdullahbakla7323@users.noreply.github.com>
2025-03-16 13:58:44 +0300

    Update ex002.py

--- Exercises/ex002.py
+++ Exercises/ex002.py
@@ -1,2 +1,7 @@
-# Your Student ID:
-# Your Name and Surname:
+# Your Student ID:230543003  
+# Your Name and Surname:Abdullah Bakla
+
+import sys
+
+print("Python version:", sys.version)
+print("Version info:", sys.version_info)
